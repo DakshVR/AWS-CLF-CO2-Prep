a0f0ffcf76e17a8fdb08f4720e234aae2d74c3e0
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-11 13:02:19 -0400

    Cloud computing

---
+++ Cloud Computing/cloudComputing.py
@@ -0,0 +1,34 @@
+
+#! How websites work?
+# 
+#*  Client ------>  Network ------>  Server
+# 
+#   Has IP                          Has IP 
+
+#  Just like sending post mail, you have address and your friend has address and the network is post office.
+
+#! What is server composed of?
+#   Computer: CPU ----|
+#                     |---- Brain
+#     Memory: RAM ----|
+
+#   Storage: Data (files) 
+#   Database: Store data in a structured way
+
+#   Network: Routers, switch, DNS server
+
+#! Terminology
+# Network: Cables, routers, and servers connected with each other
+# Routers: A networking device that forwards data packets between computer networks. They know where to send your packets on the internet!
+# Switch: Takes a packet and send it to the correct server/client on your network.
+
+#! Problems with traditional IT approach
+# Pay for the rent for the data center
+# Pay for power supply, cooling, maintenance
+# Adding and replacing hardware takes time
+# Scaling is limited
+# Hire 24/7 team to monitor the infrastructure
+# How to deal with disasters? (Power shutdowns, earthquakes, etc)
+
+#* Can we externalize all this?
+#   Yes, CLOUD
