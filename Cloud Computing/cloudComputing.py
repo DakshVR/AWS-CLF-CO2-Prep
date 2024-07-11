8689233af570438def2608cf0bbf0447fbfcfe54
Daksh Viradiya <dakshviradiya77@gmail.com>
2024-07-11 13:31:42 -0400

    advantages and problems solved by cloud

--- Cloud Computing/cloudComputing.py
+++ Cloud Computing/cloudComputing.py
@@ -1,34 +1,47 @@
 
-#! How websites work?
-# 
-#*  Client ------>  Network ------>  Server
-# 
-#   Has IP                          Has IP 
+#! What is Cloud Computing?
+#  Cloud computing is the ON-DEMAND delivery of computer power, database storage, applications, and other IT resources.
+# Through a cloud services platform with pay-as-you-go pricing
+# You can provision, right type and size of resources that cater your needs.
+# You can access the resources that you need whenever you need to
+# It is a easier method of accessing all the services that are needed.
 
-#  Just like sending post mail, you have address and your friend has address and the network is post office.
+#* AWS owns and maintains these.
 
-#! What is server composed of?
-#   Computer: CPU ----|
-#                     |---- Brain
-#     Memory: RAM ----|
+#? DropBox: Cloud storage services
+#  Originally built on AWS
 
-#   Storage: Data (files) 
-#   Database: Store data in a structured way
+#? Netflix: Built on AWS
+#  Video on demand
 
-#   Network: Routers, switch, DNS server
+#! Models of the cloud
+# Private cloud: Cloud services used by a single organization, that cater specific business needs, completely controlled.
 
-#! Terminology
-# Network: Cables, routers, and servers connected with each other
-# Routers: A networking device that forwards data packets between computer networks. They know where to send your packets on the internet!
-# Switch: Takes a packet and send it to the correct server/client on your network.
+# Pulic Cloud: MS Azure, Goolgle cloud, and AWS
 
-#! Problems with traditional IT approach
-# Pay for the rent for the data center
-# Pay for power supply, cooling, maintenance
-# Adding and replacing hardware takes time
-# Scaling is limited
-# Hire 24/7 team to monitor the infrastructure
-# How to deal with disasters? (Power shutdowns, earthquakes, etc)
+# Hybrid Cloud: keep some on land, and some on Cloud.
 
-#* Can we externalize all this?
-#   Yes, CLOUD
+
+#! 5 characteristics of Cloud computing
+#   On Demand self service: we can use resource and use them without having to interact with a person from the service provider and discuss.
+
+#   Broad network access: the resources available over the network can be access by clients or users from various platforms.
+
+#   Multi-tenancy and resource pooling: Everyone or most of us share the same infrastructure and application resources with security and privacy.
+
+#   Rapid elasticity anf scalability: Use whenever needed, if demand is high use more if not dispose what you don't need instantly.
+
+#   Measured service: you only pay for what you use.
+
+#! Advantages of Cloud Computing
+# Trade capital expense (CAPEX) for operational expense (OPEX)
+#     don't purchase hardware, rent it so you only have to pay when you need it.
+#     reduce the Total Cost of Ownership (TCO) & Operational Expense (OPEX)
+# As AWS scales the prices will go down.
+# No need to guess the capacity
+# Increase speed and agility
+# No spending of money running and maintaining data centers physically(in house)
+# By using AWs you can go world wide within a short span of time
+
+#! Problems solved by Cloud?
+# Flexibility, Cost-Effectiveness, Scalability, Elasticity, High-availability and fault-tolerance, Agility, you can develop software applications, test and launch rapidly.
